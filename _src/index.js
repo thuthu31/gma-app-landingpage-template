237a9bbff7c9a75784f79068ff2eab953e108448
thuthu31 <45553437+thuthu31@users.noreply.github.com>
2020-09-09 15:22:34 +0630

    remove darkmode

--- _src/index.js
+++ _src/index.js
@@ -8,9 +8,3 @@ const tobi = new Tobi()
 import FloatingPrompt from 'producthunt-floating-prompt'
 FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
 
-// Remove the following lines to remove the darkmode js
-import Darkmode from 'darkmode-js'
-function addDarkmodeWidget() {
-  new Darkmode().showWidget()
-}
-window.addEventListener('load', addDarkmodeWidget)
